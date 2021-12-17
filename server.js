06511bb6b536ad3fdc85f81d3f7cf9803aeff0f2
Vitor Rafael <vitor.45678.rafael@gmail.com>
2021-12-17 13:45:32 -0300

    add index.ejs to views

--- server.js
+++ server.js
@@ -6,4 +6,5 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-app.listen(8080);
+app.listen(8080); //ouve a porta 8080, o mesmo que dizer ao servidor que ao abrir o app ele deve ser aberto na porta 8080
+console.log("Running");
