52c220a1b595848243d0fde8f1ba0c7c1a8749b1
Vitor Rafael <vitor.45678.rafael@gmail.com>
2021-12-17 14:10:09 -0300

    move content files to partials move pages files to pages change server.js app directory

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("pages/index");
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("pages/about");
 });
 
 app.listen(8080); //ouve a porta 8080, o mesmo que dizer ao servidor que ao abrir o app ele deve ser aberto na porta 8080
